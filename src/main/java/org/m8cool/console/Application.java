ca9d595fcc6d8fc290bd3edef5b181c1fc4318a0
njwangxi <njwangxi@jd.com>
2017-12-04 18:37:10 +0800

    add basic spring config

---
+++ src/main/java/org/m8cool/console/Application.java
@@ -0,0 +1,15 @@
+package org.m8cool.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by m8cool on 2017/6/17.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String args[]) throws Exception{
+        SpringApplication.run(Application.class,args) ;
+    }
+}
